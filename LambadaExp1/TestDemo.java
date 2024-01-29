29eddc380186aa43821b9de7fd8d7925f8fbca63
OnkarMalawade <132775903+OnkarMalawade@users.noreply.github.com>
2024-01-29 19:24:43 +0530

    Create TestDemo.java

---
+++ LambadaExp1/TestDemo.java
@@ -0,0 +1,11 @@
+package myPack;
+
+public class TestDemo{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SingleMethod ms2 = ()->System.out.println("Hello Lambda");
+		ms2.msg();
+	}
+
+}
